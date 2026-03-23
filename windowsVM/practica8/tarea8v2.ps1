50803884f3c869858708e520f195378cea26d9d9
Karol <annaosro2005@gmail.com>
2026-03-23 14:05:54 -0700

    cambio de ruta :/ 1

--- windowsVM/practica8/tarea8v2.ps1
+++ windowsVM/practica8/tarea8v2.ps1
@@ -10,7 +10,7 @@
 # -----------------------------------------------
 $DOMINIO       = "empresa.local"
 $DC_PATH       = "DC=empresa,DC=local"
-$RUTA_CSV      = "C:\usuarios.csv"
+$RUTA_CSV      = "csv8.csv"
 $RUTA_PERFILES = "C:\Perfiles"
 $PASSWORD      = "P@ssw0rd123!"
 
